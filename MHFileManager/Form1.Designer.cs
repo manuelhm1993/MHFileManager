6ca985e36fdd1d3967347524313f35c1ebc12d8c
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-01 17:37:15 -0400

    Creación de los botones para seleccionar el origen y el destino de las carpetas o archivos, se utilizó lookup table para reutilizar la lógica en ambos eventos

--- MHFileManager/Form1.Designer.cs
+++ MHFileManager/Form1.Designer.cs
@@ -28,13 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOrigen = new System.Windows.Forms.Button();
+            this.btnDestino = new System.Windows.Forms.Button();
+            this.txtOrigen = new System.Windows.Forms.TextBox();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOrigen
+            // 
+            this.btnOrigen.Location = new System.Drawing.Point(54, 50);
+            this.btnOrigen.Name = "btnOrigen";
+            this.btnOrigen.Size = new System.Drawing.Size(107, 57);
+            this.btnOrigen.TabIndex = 0;
+            this.btnOrigen.Text = "Origen";
+            this.btnOrigen.UseVisualStyleBackColor = true;
+            this.btnOrigen.Click += new System.EventHandler(this.origen_Click);
+            // 
+            // btnDestino
+            // 
+            this.btnDestino.Location = new System.Drawing.Point(54, 139);
+            this.btnDestino.Name = "btnDestino";
+            this.btnDestino.Size = new System.Drawing.Size(107, 57);
+            this.btnDestino.TabIndex = 1;
+            this.btnDestino.Text = "Destino";
+            this.btnDestino.UseVisualStyleBackColor = true;
+            this.btnDestino.Click += new System.EventHandler(this.btnDestino_Click);
+            // 
+            // txtOrigen
+            // 
+            this.txtOrigen.Location = new System.Drawing.Point(167, 65);
+            this.txtOrigen.Name = "txtOrigen";
+            this.txtOrigen.Size = new System.Drawing.Size(326, 26);
+            this.txtOrigen.TabIndex = 2;
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.Location = new System.Drawing.Point(167, 154);
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.Size = new System.Drawing.Size(326, 26);
+            this.txtDestino.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(802, 370);
+            this.Controls.Add(this.txtDestino);
+            this.Controls.Add(this.txtOrigen);
+            this.Controls.Add(this.btnDestino);
+            this.Controls.Add(this.btnOrigen);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MHFileManager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnOrigen;
+        private System.Windows.Forms.Button btnDestino;
+        private System.Windows.Forms.TextBox txtOrigen;
+        private System.Windows.Forms.TextBox txtDestino;
     }
 }
 
--- MHFileManager/Form1.cs
+++ MHFileManager/Form1.cs
@@ -16,5 +16,38 @@ namespace MHFileManager
         {
             InitializeComponent();
         }
+
+        private void SetDirectoryPath(Button btnClick)
+        {
+            // Esta sintaxis de using permite que un objeto que usa recursos del SO se libere con Dispose() automáticamente
+            using (FolderBrowserDialog dialogo = new FolderBrowserDialog())
+            {
+                // Lookup table o mapeo de objetos
+                Dictionary<string, Dictionary<bool, Control>> lookup = new Dictionary<string, Dictionary<bool, Control>>
+                {
+                    { "btnOrigen", new Dictionary<bool, Control> { { false, txtOrigen } } },
+                    { "btnDestino", new Dictionary<bool, Control> { { true, txtDestino } } }
+                };
+
+
+                dialogo.Description = "Selecciona la carpeta de origen";
+                dialogo.ShowNewFolderButton = lookup[btnClick.Name].Keys.ElementAt(0);
+
+                if (dialogo.ShowDialog() == DialogResult.OK)
+                {
+                    lookup[btnClick.Name].Values.ElementAt(0).Text = dialogo.SelectedPath;
+                }
+            }
+        }
+
+        private void origen_Click(object sender, EventArgs e)
+        {
+            SetDirectoryPath((Button)sender);
+        }
+
+        private void btnDestino_Click(object sender, EventArgs e)
+        {
+            SetDirectoryPath((Button)sender);
+        }
     }
 }
